7a815920a5623557f48ff1dd2a9716082e322680
Cardoso55 <gacds.0308@gmail.com>
2025-10-18 12:50:21 -0300

    Front da pg gestão de estoque feito

--- app/views/estoque.php
+++ app/views/estoque.php
@@ -1,7 +1,57 @@
-<?php
-// =======================================
-// Página: estoque.php
-// Função: Controle geral do estoque.
-// Permite registrar entrada e saída de produtos,
-// visualizar quantidades, e acessar os checklists de conferência.
-// =======================================
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estoque</title>
+    <link rel="stylesheet" href="/TCC/public/css/reset.css">
+    <link rel="stylesheet" href="/TCC/public/css/sidebar.css">
+    <link rel="stylesheet" href="/TCC/public/css/estoque.css">
+</head>
+<body>
+    <div class="all">
+        <?php
+            include 'partials/sidebar.php'; 
+        ?>
+        <div class="main-content">
+
+            <h2 class="title">Gestão de Estoque</h2>
+
+            <div class="stock-management">
+                <input type="text" placeholder="Pesquisar">
+                <button>Filtrar</button>
+            </div>
+            
+            <div class="product-list">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Nome</th>
+                            <th>Tipo</th>
+                            <th>Preço Unitário</th>
+                            <th>Quantidade</th>
+                            <th><button>Adicionar</button></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>01</td>
+                            <td>Água</td>
+                            <td>Consumível</td>
+                            <td>R$ 3,00</td>
+                            <td>50</td>
+                            <td>
+                                <i class="edit-icon"></i>
+                                <i class="delete-icon"></i>
+                                <i class="alert-icon"></i>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
